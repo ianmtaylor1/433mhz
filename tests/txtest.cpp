11e3ab43bde736f43e32e91b6747774c7776a506
Ian Taylor <ian.m.taylor1@gmail.com>
2015-04-01 01:01:35 -0400

    Added two tests: using different protocol to send same code - success. creating new code (not on remote) and training outlet with it - success.

--- tests/txtest.cpp
+++ tests/txtest.cpp
@@ -8,14 +8,20 @@
 #include <wiringPi.h>
 
 using RFTools::protocol1;
+using RFTools::tristate;
 using RFTools::Transmitter;
 using RFTools::Transmission;
 
 int main(int argc, char **argv)
 {
-    Transmission on = protocol1(1144067,24,186);
+    Transmission on  = protocol1(1144067,24,186);
     Transmission off = protocol1(1144076,24,186);
     
+    Transmission t_on  = tristate("0F0FF1FF0001",186);
+    Transmission t_off = tristate("0F0FF1FF0010",186);
+    
+    Transmission test_on  = tristate("0F0FFFFF0001",186);
+    Transmission test_off = tristate("0F0FFFFF0010",186);
     //std::cout << on.size() << "\n";
     //std::cout << off.size() << "\n";
     
@@ -23,10 +29,22 @@ int main(int argc, char **argv)
     
     Transmitter tx = Transmitter(0);
     
-    for (int i=0; i<10; i++) {
+    for (int i=0; i<3; i++) {
         tx.transmit(on);
         sleep(1);
         tx.transmit(off);
+        sleep(1);
+    }
+    for (int i=0; i<3; i++) {
+        tx.transmit(t_on);
+        sleep(1);
+        tx.transmit(t_off);
+        sleep(1);
+    }
+    for (int i=0; i<3; i++) {
+        tx.transmit(test_on);
+        sleep(1);
+        tx.transmit(test_off);
         sleep(1);
     }
 
