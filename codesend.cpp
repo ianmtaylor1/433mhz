2767b4bbcb0f49bd0c149b54275f17a72dec5bd6
Ian Taylor <ian.m.taylor1@gmail.com>
2015-04-03 23:01:30 -0400

    Minor changes to codesend.cpp. Should I use getopt for options?

--- codesend.cpp
+++ codesend.cpp
@@ -1,8 +1,15 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
 #include "433mhz.h"
 
 int main(int argc, char **argv)
 {
+    int pulselength;
+    std::string code;
+    int repeat;
     (void) argc;
     (void) argv;
     return 0;
 }
+
