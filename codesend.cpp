a62076938175a509296b873f727d3cc286c2cc3f
Ian Taylor <ian.m.taylor1@gmail.com>
2015-04-06 23:25:18 -0400

    Added basic use of TCLAP in codesend.cpp

--- codesend.cpp
+++ codesend.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
+
+#include <tclap/CmdLine.h>
+
 #include "433mhz.h"
 
 
 int main(int argc, const char *argv[])
 {
+    //construct the command line parser
+    TCLAP::CmdLine cmd("Sends a code on the 433mhz transmitter.",' ',"0.0");
     int pulselength;
     std::string code;
     int repeat;
+    cmd.parse(argc, argv);
     return 0;
 }
 
